57e3b27203776460e8295726dc3aa678e8197bd8
Ethan Hansen <hanseneg@hotmail.com>
2020-11-02 09:39:10 -0700

    commit5

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ function Footer(){
     return (
         <div>
             <span className="footer">
-                <img className="icons" src={"https://image.flaticon.com/icons/svg/1384/1384017.svg"}/>
-                <img className="icons" src={"https://image.flaticon.com/icons/svg/49/49354.svg"}/>
-                <img className="icons" src={"https://image.flaticon.com/icons/svg/2111/2111432.svg"}/>
+                <img className="icons" alt='' src={"https://image.flaticon.com/icons/svg/1384/1384017.svg"}/>
+                <img className="icons" alt='' src={"https://image.flaticon.com/icons/svg/49/49354.svg"}/>
+                <img className="icons" alt='' src={"https://image.flaticon.com/icons/svg/2111/2111432.svg"}/>
             </span>
             <p className="footer">Copyright © Your Website 2020</p>
         </div>
